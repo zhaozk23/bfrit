79311636813af0646a3180f652eee92369e96e1a
zzk <zzk550994@antgroup.com>
2026-08-05 16:00:27 +0800

    fix jump errors

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use libc::PROT_WRITE;
 use std::env;
 use std::fs;
 use std::io::{Read, Write};
+use std::process;
 use std::ptr;
 enum Op {
     Inc(usize),
@@ -81,19 +82,64 @@ impl<'a> Lexer<'a> {
 }
 struct Backpatch {
     addr: usize,
-    src_index: usize,
+    src_addr: usize,
     dst_index: usize,
 }
 impl Backpatch {
-    fn new(addr: usize, src_index: usize, dst_index: usize) -> Self {
+    fn new(addr: usize, src_addr: usize, dst_index: usize) -> Self {
         Self {
             addr,
-            src_index,
+            src_addr,
             dst_index,
         }
     }
 }
-type Code = fn(*mut u8) -> ();
+fn parse(code: &str) -> Result<Vec<Op>, ()> {
+    let mut ops = Vec::new();
+    let mut addr_stack = Vec::new();
+    let mut lexer = Lexer::new(code);
+    while let Some(ch) = lexer.next() {
+        match ch {
+            b'+' | b'-' | b'<' | b'>' | b',' | b'.' => {
+                let mut cnt = 1;
+                while let Some(s) = lexer.peek() {
+                    if s == ch {
+                        lexer.next();
+                        cnt += 1;
+                    } else {
+                        break;
+                    }
+                }
+                ops.push(Op::new(ch, cnt));
+            }
+            b'[' => {
+                let addr = ops.len();
+                ops.push(Op::new(ch, 0));
+                addr_stack.push(addr);
+            }
+            b']' => {
+                let addr = addr_stack.pop().ok_or_else(|| {
+                    eprintln!("Unmatched closing bracket at position {}.\n", lexer.pos);
+                })?;
+                ops.push(Op::new(ch, addr + 1));
+                ops[addr] = Op::new(b'[', ops.len());
+            }
+            _ => {
+                eprintln!("Invalid character in Brainfuck code: {}", ch);
+                return Err(());
+            }
+        }
+    }
+    if !addr_stack.is_empty() {
+        eprintln!(
+            "Unmatched opening bracket(s) at positions: {:?}\n",
+            addr_stack
+        );
+        return Err(());
+    }
+    Ok(ops)
+}
+type Code = extern "C" fn(*mut u8);
 #[allow(dead_code)]
 fn interpret(ops: &[Op]) {
     let mut memory = vec![0u8];
@@ -111,10 +157,11 @@ fn interpret(ops: &[Op]) {
             }
             Op::Left(cnt) => {
                 if head < cnt {
-                    panic!(
+                    eprintln!(
                         "ERROR: Memory underflow at instruction {}: moving left by {} from position {}.",
                         ip, cnt, head
                     );
+                    process::exit(1);
                 }
                 head = head.saturating_sub(cnt);
                 ip += 1;
@@ -127,6 +174,7 @@ fn interpret(ops: &[Op]) {
                 ip += 1;
             }
             Op::Input(_) => {
+                std::io::stdout().flush().unwrap();
                 let mut buffer = [0u8; 1];
                 std::io::stdin().read_exact(&mut buffer).unwrap();
                 memory[head] = buffer[0];
@@ -135,7 +183,6 @@ fn interpret(ops: &[Op]) {
             Op::Output(cnt) => {
                 for _ in 0..cnt {
                     print!("{}", memory[head] as char);
-                    std::io::stdout().flush().unwrap();
                 }
                 ip += 1;
             }
@@ -155,12 +202,13 @@ fn interpret(ops: &[Op]) {
             }
         }
     }
+    std::io::stdout().flush().unwrap();
 }
 fn jit_compile(ops: &[Op]) -> Code {
     let mut code: Vec<u8> = Vec::new();
     let mut backpatches: Vec<Backpatch> = Vec::new();
     let mut addrs: Vec<usize> = Vec::new();
-    for (i, op) in ops.iter().enumerate() {
+    for op in ops {
         addrs.push(code.len());
         match op {
             Op::Inc(value) => {
@@ -187,7 +235,17 @@ fn jit_compile(ops: &[Op]) -> Code {
                 let value = *value as u32;
                 code.extend_from_slice(&value.to_le_bytes());
             }
-            Op::Input(_) => todo!(),
+            Op::Input(cnt) => {
+                for _ in 0..*cnt {
+                    code.push(0x57); // push rdi
+                    code.extend_from_slice(&[0x48, 0xC7, 0xC0, 0x00, 0x00, 0x00, 0x00]); // mov rax, 0
+                    code.extend_from_slice(&[0x48, 0x89, 0xFE]); // mov rsi, rdi
+                    code.extend_from_slice(&[0x48, 0xC7, 0xC7, 0x00, 0x00, 0x00, 0x00]); // mov rdi, 0
+                    code.extend_from_slice(&[0x48, 0xC7, 0xC2, 0x01, 0x00, 0x00, 0x00]); // mov rdx, 1
+                    code.extend_from_slice(&[0x0F, 0x05]); // syscall
+                    code.push(0x5F); // pop rdi
+                }
+            }
             Op::Output(cnt) => {
                 for _ in 0..*cnt {
                     code.push(0x57); // push rdi
@@ -200,30 +258,29 @@ fn jit_compile(ops: &[Op]) -> Code {
                 }
             }
             Op::Jz(value) => {
-                code.extend_from_slice(&[0x48, 0x31, 0xC0]); // xor rax, rax
                 code.extend_from_slice(&[0x8A, 0x07]); // mov al, byte ptr [rdi]
-                code.extend_from_slice(&[0x48, 0x85, 0xC0]); // test rax, rax
+                code.extend_from_slice(&[0x84, 0xC0]); // test al, al
                 code.extend_from_slice(&[0x0F, 0x84]); // jz rel32
                 let op_addr = code.len();
-                code.extend_from_slice(&[0x00, 0x00, 0x00, 0x00]); // placeholder for jump address
-                backpatches.push(Backpatch::new(op_addr, i, *value));
+                code.extend_from_slice(&[0x00; 4]); // placeholder for jump address
+                let next_ip = code.len();
+                backpatches.push(Backpatch::new(op_addr, next_ip, *value));
             }
             Op::Jnz(value) => {
-                code.extend_from_slice(&[0x48, 0x31, 0xC0]); // xor rax, rax
                 code.extend_from_slice(&[0x8A, 0x07]); // mov al, byte ptr [rdi]
-                code.extend_from_slice(&[0x48, 0x85, 0xC0]); // test rax, rax
+                code.extend_from_slice(&[0x84, 0xC0]); // test al, al
                 code.extend_from_slice(&[0x0F, 0x85]); // jnz rel32
                 let op_addr = code.len();
-                code.extend_from_slice(&[0x00, 0x00, 0x00, 0x00]); // placeholder for jump address
-                backpatches.push(Backpatch::new(op_addr, i, *value));
+                code.extend_from_slice(&[0x00; 4]); // placeholder for jump address
+                let next_ip = code.len();
+                backpatches.push(Backpatch::new(op_addr, next_ip, *value));
             }
         }
     }
     addrs.push(code.len());
     for bp in backpatches {
-        let src_addr = addrs[bp.src_index];
         let dst_addr = addrs[bp.dst_index];
-        let rel32 = (dst_addr as isize - (src_addr as isize + 4)) as i32;
+        let rel32 = (dst_addr as isize - bp.src_addr as isize) as i32;
         let rel32_bytes = rel32.to_le_bytes();
         code[bp.addr..bp.addr + 4].copy_from_slice(&rel32_bytes);
     }
@@ -239,7 +296,8 @@ fn jit_compile(ops: &[Op]) -> Code {
             0,
         );
         if ptr == libc::MAP_FAILED {
-            panic!("mmap failed");
+            eprintln!("mmap failed");
+            process::exit(1);
         }
         ptr::copy_nonoverlapping(code.as_ptr(), ptr as *mut u8, len);
         std::mem::transmute::<*mut u8, Code>(ptr as *mut u8)
@@ -247,49 +305,42 @@ fn jit_compile(ops: &[Op]) -> Code {
 }
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        eprintln!("Usage: {} <brainfuck_file>", args[0]);
-        std::process::exit(1);
+    if args.len() < 2 {
+        eprintln!("Usage: {} [--interpret] <brainfuck_file>", args[0]);
+        process::exit(1);
     }
-    let mut ops = Vec::new();
-    let mut addr_stack = Vec::new();
-    let filename = &args[1];
-    let code = fs::read_to_string(filename)
-        .unwrap_or_else(|e| panic!("Failed to read file {}: {}", filename, e));
-    let mut lexer = Lexer::new(&code);
-    while let Some(ch) = lexer.next() {
-        match ch {
-            b'+' | b'-' | b'<' | b'>' | b',' | b'.' => {
-                let mut cnt = 1;
-                while let Some(s) = lexer.peek() {
-                    if s == ch {
-                        lexer.next();
-                        cnt += 1;
-                    } else {
-                        break;
-                    }
-                }
-                ops.push(Op::new(ch, cnt));
-            }
-            b'[' => {
-                let addr = ops.len();
-                ops.push(Op::new(ch, 0));
-                addr_stack.push(addr);
-            }
-            b']' => {
-                let addr = addr_stack.pop().unwrap_or_else(|| {
-                    panic!("Unmatched closing bracket at position {}.\n", lexer.pos)
-                });
-                ops.push(Op::new(ch, addr));
-                ops[addr] = Op::new(b'[', ops.len());
-            }
-            _ => panic!("Invalid character in Brainfuck code: {}", ch),
+    let mut interpret_mode = false;
+    let mut filename = None;
+    for arg in &args[1..] {
+        if arg == "--interpret" {
+            interpret_mode = true;
+        } else if filename.is_none() {
+            filename = Some(arg);
+        } else {
+            eprintln!("Invalid argument: {}", arg);
+            process::exit(1);
         }
     }
+    let filename = filename.unwrap_or_else(|| {
+        eprintln!("No input file specified.");
+        process::exit(1);
+    });
+    let code = fs::read_to_string(filename).unwrap_or_else(|e| {
+        eprintln!("Failed to read file {}: {}", filename, e);
+        process::exit(1);
+    });
+    let ops = parse(&code).unwrap_or_else(|_| {
+        eprintln!("Failed to parse Brainfuck code.");
+        process::exit(1);
+    });
     // for (i, op) in ops.iter().enumerate() {
     //     println!("{}: {}", i, op);
     // }
-    let code = jit_compile(&ops);
-    let mut memory = vec![0u8; 1024];
-    code(memory.as_mut_ptr());
+    if interpret_mode {
+        interpret(&ops);
+    } else {
+        let code = jit_compile(&ops);
+        let mut memory = vec![0u8; 1_000_000];
+        code(memory.as_mut_ptr());
+    }
 }
